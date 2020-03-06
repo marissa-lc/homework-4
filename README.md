a0350ebca92a2ef2bdcf6d0bff0b96a871370130
Marissa Crawford <marissa.linn.crawford@gmail.com>
2020-03-05 23:06:26 -0800

    updating quiz hw

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # homework-4
 Code Quiz
+
+This is a timed quiz to help freshen up your javascript knowledge.
+
+The goal of this game is to answer all of the questions correctly before the time runs out. Wrong answers will result in loss of time.
+
+You have the option of viewing your time and the high score as you play the game.
+
+Once the quiz is completed you have the option of entering in your initials to save your high score
--- index.js
+++ index.js
@@ -1,85 +1,47 @@
 
-
 $(document).ready(function(){
     
-    $("#start").click(function(){
-        $("div").remove("");
-      });
-
-//Code should go here
-
-  //Store the total number of questions
-  var totalQuestions = $('.questions').size(6);
-
-  //Set the current question to display to 1
-  var currentQuestion = 1;
-  
-  //Store the selector in a variable.
-  //It is good practice to prefix jQuery selector variables with a $
-  $questions = $('.questions');
-  
-  //Hide all the questions
-  $questions.hide();
-  
-  //Show the first question
-  $($questions.get(currentQuestion)).fadeIn();
-  
-  //attach a click listener to the HTML element with the id of 'next'
-  $('#next').click(function () {
-  
-       //fade out the current question,
-       //putting a function inside of fadeOut calls that function 
-       //immediately after fadeOut is completed, 
-       //this is for a smoother transition animation
-       $($questions.get(currentQuestion)).fadeOut(function () {
-  
-          //increment the current question by one
-          currentQuestion = currentQuestion + 1;
-  
-          //if there are no more questions do stuff
-          if (currentQuestion == totalQuestions) {
-  
-              var result = sum_values()
-  
-              //do stuff with the result
-              alert(result);
-  
-          } else {
-  
-              //otherwise show the next question
-              $($questions.get(currentQuestion)).fadeIn();
-  
-          }
-      });
-  
-  });
+  $("#start").click(function(){
+      $("div").remove("");
+    });
 
 
+var totalQuestions = $('.questions').size(6);
 
+var currentQuestion = 0;
 
+$questions = $('.questions');
 
+//Hide all the questions
+$questions.hide();
 
+//Show the first question
+$($questions.get(currentQuestion)).fadeIn();
 
+//attach a click listener to the HTML element with the id of 'next'
+$('#next').click(function () {
 
+     $($questions.get(currentQuestion)).fadeOut(function () {
 
-  });
+        //increment the current question by one
+        currentQuestion = currentQuestion + 1;
 
+        //if there are no more questions do stuff
+        if (currentQuestion == totalQuestions) {
 
+            var result = sum_values()
 
+            //do stuff with the result
+            alert(result);
 
+        } else {
 
-// Store your questions as an array of objects in a separate file, `questions.js`, that follows this format:
+            //otherwise show the next question
+            $($questions.get(currentQuestion)).fadeIn();
 
-// // var questions = [
-//   {
-//     title: "Commonly used data types DO NOT include:",
-//     choices: ["strings", "booleans", "alerts", "numbers"],
-//     answer: "alerts"
-//   },
-//   {
-//     title: "The condition in an if / else statement is enclosed within ____.",
-//     choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-//     answer: "parentheses"
-//   },
-//   ///etc.
-// ];
+        }
+    });
+
+});
+
+});
